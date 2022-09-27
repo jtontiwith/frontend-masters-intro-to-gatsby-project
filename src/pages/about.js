5cfb1759b734cd5fdcad5696c28de551350ed3c0
Jonathan Tonti <jt@Jonathans-MacBook-Air.local>
2022-09-27 14:27:05 -0500

    create pages

---
+++ src/pages/about.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const about = () => {
+    return (
+        <div>
+          About here
+        </div>
+    );
+};
+
+export default about;
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const IndexPage = () => {
+  return (
+    <main> 
+      <h1>Yo, Gatsby here.</h1>
+    </main>
+  )
+}
+
+export default IndexPage
